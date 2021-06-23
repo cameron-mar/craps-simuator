814fe905788253050b2caacefecf8d1c52713dd9
Cameron Mar <cameron.mar13@gmail.com>
2021-06-22 22:20:06 -0700

    Create Gatsby Shell

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+const IndexPage = () => (
+  <div>
+    
+  </div>
+)
+
+export default IndexPage
